57bc247782deb20c3c378beb73dae6bef2df2325
Bala <balavignarajan63@gmail.com>
2026-03-19 12:55:12 +0530

    sumbit form added back btn

--- app/(unauthenticated)/interest-form.tsx
+++ app/(unauthenticated)/interest-form.tsx
@@ -229,6 +229,7 @@ export default function InterestFormScreen() {
                     showNotifications={false}
                     showUserName
                     customTitle="Interest Form"
+                    showBackButton={true}
                 />
 
                 <KeyboardAwareScrollView
